9f41594c0390132d83d59dfc93852b47f247a511
rekakojuzzo <aoyheart33@gmail.com>
2025-07-27 18:59:14 +0300

    v1.3

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,5 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/reykodev/',
   plugins: [react()],
-})
 })
